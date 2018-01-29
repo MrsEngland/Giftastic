ccd9a3d8f276d11b06141f0f2ac8199d455d330c
Donna England <mrsengland@aol.com>
2018-01-28 22:17:18 -0500

    homework

---
+++ assets/giftastic.js
@@ -0,0 +1,3 @@
+// i tried to put the javascript code in this file. I copied all the code between <script> and </script> 
+// When I did this it would not function properly.  I added the link correctly as far as I can tell. 
+//please advise. 
